0d76cad8a4c12ef56e1acc1334db0ef57b357d45
Frederico Cruz <datacruzsistemas@gmail.com>
2018-05-21 09:12:14 -0300

    estudando...

---
+++ br.com.datacruz/Carro.java
@@ -0,0 +1,14 @@
+//package br.com.datacruz;
+
+import br.com.datacruz.rodas;
+
+public class Carro{
+	static String MOTOR = "AT";
+
+	public static void main(String ... args){
+
+			
+
+		System.out.println( MOTOR );
+	}
+}
